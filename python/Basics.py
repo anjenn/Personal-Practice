3aee9fcc465338a9aa2915c983488e9c6357da4e
anjenn <93240896+anjenn@users.noreply.github.com>
2022-03-25 20:56:59 +0100

    python basics

---
+++ python/Basics.py
@@ -0,0 +1,16 @@
+print("Hello world")
+
+# working with strings
+
+# no semicolon needed
+# \" when we want to print a quotation mark
+# \n new line
+
+
+phrase = "some text"
+print("this is\n sthsth" + phrase)
+print(phrase.upper().isupper())
+print(phrase.upper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index("e"))
