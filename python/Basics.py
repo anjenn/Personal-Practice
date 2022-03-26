3da8ddb43bc233c49c6c15a1cd27070f488804ba
anjenn <93240896+anjenn@users.noreply.github.com>
2022-03-26 18:51:20 +0100

    working with numbers

--- python/Basics.py
+++ python/Basics.py
@@ -6,7 +6,6 @@ print("Hello world")
 # \" when we want to print a quotation mark
 # \n new line
 
-
 phrase = "some text"
 print("this is\n sthsth" + phrase)
 print(phrase.upper().isupper())
@@ -14,3 +13,9 @@ print(phrase.upper())
 print(len(phrase))
 print(phrase[3])
 print(phrase.index("e"))
+
+# working with numbers
+print(10%3)  # and then we get the remainder
+my_num = 5
+print(str(my_num)+"my favorite number") # in order to concatenate with string, we must fist convert the number into string
+
